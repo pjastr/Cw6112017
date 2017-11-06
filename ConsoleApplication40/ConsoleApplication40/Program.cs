a358b93306fc4b9a4aeee861bebb6b1e03609602
pjastr <24464854+pjastr@users.noreply.github.com>
2017-11-06 11:36:46 +0100

    Add files via upload

---
+++ ConsoleApplication40/ConsoleApplication40/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class WlasnaKlasa:IBbb
+    {
+
+        public string Wyraz()
+        {
+            return "ssds";
+        }
+
+        public int Liczba()
+        {
+            return 3;
+        }
+    }
+
+    interface IAaa
+    {
+        int Liczba();
+    }
+
+    interface IBbb : IAaa
+    {
+        string Wyraz();
+    }
+}
